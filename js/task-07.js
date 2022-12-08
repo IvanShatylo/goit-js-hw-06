8db6e45ae63293d7b108aa087323d5cd0c768f31
Ivan Shatylo <shatilo540@gmail.com>
2022-12-08 10:48:40 +0200

    update task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,8 @@
 const inputRef = document.querySelector('#font-size-control');
 const textRef = document.querySelector('span');
 
-let textFontSize = (textRef.style.fontSize = '');
-
 inputRef.addEventListener('input', onInput);
 
 function onInput(event) {
-  let textFontSize = (textRef.style.fontSize = `${event.currentTarget.value}px`);
+  textRef.style.fontSize = inputEl.value + "px";
 }
